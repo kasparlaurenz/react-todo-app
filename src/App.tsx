464ac1166241828d6dd5d7dd9fcbcfa230051d85
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-08-17 12:21:28 +0200

    add basic layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
+import './App.scss';
 
-const App = () => {
-  return <div>App</div>;
+const App: FC = () => {
+  return (
+    <div className='app'>
+      <div className='header'>
+        <div className='input-container'>
+          <input type='text' placeholder='What should be done?' />
+          <input type='number' placeholder='Deadline in days' />
+        </div>
+        <button>Add Todo</button>
+      </div>
+      <div className='todo-list'>Todos here</div>
+    </div>
+  );
 };
 
 export default App;
